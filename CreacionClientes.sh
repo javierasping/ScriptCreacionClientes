c9b26bae6e1fbbe30e7f95baa1ec76aea672d23e
Javier Cruces <javierasping@gmail.com>
2023-10-18 09:57:12 +0200

    Correction

--- CreacionClientes.sh
+++ CreacionClientes.sh
@@ -11,19 +11,22 @@ nombre=$1
 tamaño_volumen=$2
 red=$3
 
+# Obtener el nombre del puente asociado a la red
+puente=$(virsh --connect qemu:///system net-dumpxml "${red}" | grep -m 1 -oP "<bridge name='\K[^']+")
+
 # Crear un nuevo volumen a partir de la plantilla
-virsh vol-create-as default "${nombre}.qcow2" ${tamaño_volumen}G --format qcow2 --backing-vol Plantilla-debian.qcow2 --backing-vol-format qcow2
+virsh --connect qemu:///system vol-create-as default "${nombre}.qcow2" ${tamaño_volumen}G --format qcow2 --backing-vol Plantilla-debian.qcow2 --backing-vol-format qcow2
 
 # Crear la máquina virtual
 virt-install --connect qemu:///system --virt-type kvm --name "${nombre}" --os-variant debian10 --disk path="/var/lib/libvirt/images/${nombre}.qcow2" --memory 4096 --vcpus 2 --import
 
-# Conectar la máquina virtual a la red especificada
-virsh attach-interface "${nombre}" bridge "${red}" --model virtio
+# Conectar la máquina virtual al puente asociado a la red
+virsh --connect qemu:///system attach-interface "${nombre}" bridge "${puente}" --model virtio
 
 # Cambiar el nombre del host de la máquina virtual
 virt-customize --connect "qemu:///system" --hostname "${nombre}"
 
 # Iniciar la máquina virtual
-virsh start "${nombre}"
+virsh --connect qemu:///system start "${nombre}"
 
-echo "La máquina virtual ${nombre} se ha creado y está en funcionamiento."
+echo "La máquina virtual ${nombre} se ha creado y está en funcionamiento en el puente ${puente}."
