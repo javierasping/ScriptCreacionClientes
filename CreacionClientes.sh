d2b4100cfbdb19a46dfda037b579ebf88dfce5d3
Javier Cruces <javierasping@gmail.com>
2023-10-18 10:38:26 +0200

    Correcciones

--- CreacionClientes.sh
+++ CreacionClientes.sh
@@ -8,25 +8,23 @@ fi
 
 # Variables
 nombre=$1
-tamaño_volumen=$2
+tamano_volumen=$2 
 red=$3
 
 # Obtener el nombre del puente asociado a la red
 puente=$(virsh --connect qemu:///system net-dumpxml "${red}" | grep -m 1 -oP "<bridge name='\K[^']+")
 
 # Crear un nuevo volumen a partir de la plantilla
-virsh --connect qemu:///system vol-create-as default "${nombre}.qcow2" ${tamaño_volumen}G --format qcow2 --backing-vol Plantilla-debian.qcow2 --backing-vol-format qcow2
-
-# Crear la máquina virtual
-virt-install --connect qemu:///system --virt-type kvm --name "${nombre}" --os-variant debian10 --disk path="/var/lib/libvirt/images/${nombre}.qcow2" --memory 4096 --vcpus 2 --import
-
-# Conectar la máquina virtual al puente asociado a la red
-virsh --connect qemu:///system attach-interface "${nombre}" bridge "${puente}" --model virtio
+virsh --connect qemu:///system vol-create-as default "${nombre}.qcow2" "${tamano_volumen}G" --format qcow2 --backing-vol plantilla-taller1.img --backing-vol-format qcow2
 
 # Cambiar el nombre del host de la máquina virtual
-virt-customize --connect "qemu:///system" --hostname "${nombre}"
+sudo virt-customize --connect "qemu:///system" -a "/var/lib/libvirt/images/${nombre}.qcow2" --hostname "${nombre}"
+
+# Crear la máquina virtual
+virt-install --connect qemu:///system --virt-type kvm --name "${nombre}" --os-variant debian10 --disk path="/var/lib/libvirt/images/${nombre}.qcow2",size="${tamano_volumen}",format=qcow2 --memory 4096 --vcpus 2 --import --network bridge="${puente}"
 
 # Iniciar la máquina virtual
 virsh --connect qemu:///system start "${nombre}"
 
 echo "La máquina virtual ${nombre} se ha creado y está en funcionamiento en el puente ${puente}."
+
